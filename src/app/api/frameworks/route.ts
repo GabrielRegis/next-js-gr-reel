a4c549db0369916a9c54deb2768783b08d99db0b
Gabriel Regis <gabiel@Gabriels-MacBook-Pro.local>
2023-04-17 14:13:37 -0300

    Removed static props

--- src/app/api/frameworks/route.ts
+++ src/app/api/frameworks/route.ts
@@ -1,15 +1,6 @@
 import { NextResponse } from 'next/server'
 import prisma from '../prisma';
 
-export async function getStaticProps() {
-  const frameworks = await prisma.framework.findMany()
-
-  return {
-    props: frameworks
-  }
-  
-}
-
 export async function GET(request: Request) {
   const frameworks = await prisma.framework.findMany()
   return NextResponse.json(frameworks);
